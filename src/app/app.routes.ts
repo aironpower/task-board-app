27fae07613f9f6b484527a3ade7fa1d2b181133b
Manuel Giron <manuel.giron@vixtechnology>
2026-03-18 08:00:21 +0100

    ajouter component board et configuration de lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: 'board',
+        // ¡Esta es la clave del Lazy Loading en Angular moderno!
+        loadComponent: () => import('./features/board/board.component').then(c => c.BoardComponent)
+      },
+      {
+        path: '',
+        redirectTo: 'board',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: '' // Redirigir a la página principal si la ruta no existe
+  }
+];
